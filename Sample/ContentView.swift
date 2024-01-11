8bde9085d4cf2b9d744cd1c4de4f5fd3a1edeb38
Esakkiraja <esakkiraja.p@rakuten.com>
2024-01-11 17:52:04 +0530

    feat: add sample app target & scheme (SDKCF-6856)

---
+++ Sample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by P, Esakkiraja on 11/01/24.
+//  Copyright © 2024 Rakuten, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample/SampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by P, Esakkiraja on 11/01/24.
+//  Copyright © 2024 Rakuten, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
